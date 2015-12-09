792e700ead4cb916e78c434c4e971cbea9e317e8
Matt Warren <matt.warren@gmail.com>
2015-12-09 07:41:41 -0700

    update licence

--- mu/api.py
+++ mu/api.py
@@ -1,4 +1,4 @@
-# Copyright 2015 by Halotis, Inc.
+# Copyright 2015 by Matt Warren <matt.warren@gmail>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
